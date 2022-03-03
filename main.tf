4a3ccbd42b791db1a23b9298c7aac835458cb6ed
Fiifi Sam <fbsbusiness456@gmail.com>
2022-03-03 14:32:30 -0500

    Done

--- main.tf
+++ main.tf
@@ -130,7 +130,11 @@ resource "aws_eip" "one" {
   vpc                       = true
   network_interface         = aws_network_interface.test-nic.id
   associate_with_private_ip = "10.0.1.50"
-  
+  depends_on = [aws_internet_gateway.gw]
+    
+  tags = {
+    Name = "one_eip"
+  }
 }
 
 #9. Create a Amazon Linux 2 server
